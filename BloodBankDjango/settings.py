560b077292efc373a7125890029f67c2217bf8cd
Sandhanakrishnan <Sandhanakrishnan@Chandrus-MacBook-Air.local>
2020-11-22 08:52:49 +0530

    l2

--- BloodBankDjango/settings.py
+++ BloodBankDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'BloodBankDjango.urls'
