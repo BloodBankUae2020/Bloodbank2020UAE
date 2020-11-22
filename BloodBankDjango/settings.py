0755591fbd3fb8b3395baac6e84d25462bf78d65
Sandhanakrishnan <Sandhanakrishnan@Chandrus-MacBook-Air.local>
2020-11-22 08:17:47 +0530

    n1

--- BloodBankDjango/settings.py
+++ BloodBankDjango/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteeNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'BloodBankDjango.urls'
@@ -117,9 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join('/Users/Sandhanakrishnan/BBM_G/Bloodbank2020UAE', 'static'),
-)
+STATICFILES_DIRS= [os.path.join (BASE_DIR,'static')]
+
 
